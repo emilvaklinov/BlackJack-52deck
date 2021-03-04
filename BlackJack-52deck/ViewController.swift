4ce86fc28dd69df1e8a73a8cbd32e3037801e6b1
Emil Vaklinov <emil_vaklinov@yahoo.com>
2021-03-04 11:01:00 +0000

    Initial Commit

---
+++ BlackJack-52deck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BlackJack-52deck
+//
+//  Created by Emil Vaklinov on 04/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BlackJack-52deckTests/BlackJack_52deckTests.swift
@@ -0,0 +1,33 @@
+//
+//  BlackJack_52deckTests.swift
+//  BlackJack-52deckTests
+//
+//  Created by Emil Vaklinov on 04/03/2021.
+//
+
+import XCTest
+@testable import BlackJack_52deck
+
+class BlackJack_52deckTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
